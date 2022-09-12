c8697a707bfdbf6fa87a6b212fe7cfc8d7372085
TiffyAlva <TiffanyAlvarez54@gmail.com>
2022-09-12 18:26:24 -0400

    added array questions and function

---
+++ index.js
@@ -0,0 +1,155 @@
+const inquirer = require ("inquirer")
+const Manager = require("./lib/Manager")
+const Engineer = require("./lib/Engineer")
+const Intern = require("./lib/Intern")
+var employeeArray = []
+function mainQuestions(){
+    inquirer.prompt([{
+        type: "list",
+        name: "role",
+        message: "Which role do you like to add?",
+        choices: [
+            "Manager", "Engineer", "Intern", "None"
+        ] 
+    }])
+    
+    .then((answers)=>{
+        console.log(answers)
+        if(answers.role == "Manager"){
+            //Create Manager
+            createManager()
+        } 
+        if(answers.role == "Engineer"){
+            //Create Engineer
+            createEngineer()
+        }
+        if(answers.role == "Intern"){     //Repeat Steps
+            //Create Intern
+        }
+        if(answers.role == "None"){
+            //Generating Html //take advantage of 'employeeArray'
+        }
+    })
+}
+
+
+
+
+function createManager(){
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "name", 
+            message: "What is the Manager's name?"
+        },
+
+        {
+            type: "input",
+            name: "id",
+            message: "What's the Manager's ID?"
+        },
+
+        {
+            type:"input",
+            name:"email",
+            message:"What is the Manager's email?",
+        },
+
+        {
+            type:"input",
+            name:"officeNumber",
+            message:"What's the Manager's office number?",
+        },
+    ])
+
+    .then((answers)=>{
+        console.log(answers)
+        const newManager = new Manager(answers.name, answers.id, answers.email, answers.officeNumber)
+        console.log(newManager)
+        employeeArray.push(newManager)
+        mainQuestions()
+
+    })
+}
+
+
+function createEngineer(){
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "name", 
+            message: "What is the Engineer's name?"
+        },
+
+        {
+            type: "input",
+            name: "id",
+            message: "What's the Engineer's ID?"
+        },
+
+        {
+            type:"input",
+            name:"email",
+            message:"What is the Engineer's email?",
+        },
+
+        {
+            type:"input",
+            name:"github",
+            message:"What's the Engineer's GitHub?",
+        },
+    ])
+
+    .then((answers)=>{
+        console.log(answers)
+        const newEngineer = new Engineer(answers.name, answers.id, answers.email, answers.github)
+        console.log(newEngineer)
+        employeeArray.push(newEngineer)
+        mainQuestions()
+
+    })
+}
+
+
+function createIntern(){
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "name", 
+            message: "What is the Intern's name?"
+        },
+
+        {
+            type: "input",
+            name: "id",
+            message: "What's the Intern's ID?"
+        },
+
+        {
+            type:"input",
+            name:"email",
+            message:"What is the Intern's email?",
+        },
+
+        {
+            type:"input",
+            name:"github",
+            message:"What's the Intern's school?",
+        },
+    ])
+
+    .then((answers)=>{
+        console.log(answers)
+        const newIntern = new Intern(answers.name, answers.id, answers.email, answers.school)
+        console.log(newIntern)
+        employeeArray.push(newIntern)
+        mainQuestions()
+
+    })
+}
+
+
+
+
+
+mainQuestions()
